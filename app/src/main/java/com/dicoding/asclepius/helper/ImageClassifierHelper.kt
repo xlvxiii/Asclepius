b4d1d943d9f9dda88b9f9af2957717d18ffac621
xlvxiii <pangestualviansyah@gmail.com>
2024-11-10 01:11:12 +0700

    feat: implement cancer prediction

--- app/src/main/java/com/dicoding/asclepius/helper/ImageClassifierHelper.kt
+++ app/src/main/java/com/dicoding/asclepius/helper/ImageClassifierHelper.kt
@@ -1,18 +1,90 @@
 package com.dicoding.asclepius.helper
 
+import android.content.Context
 import android.graphics.Bitmap
-import android.media.Image
+import android.graphics.ImageDecoder
 import android.net.Uri
+import android.os.Build
+import android.os.SystemClock
+import android.provider.MediaStore
+import android.util.Log
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.support.common.ops.CastOp
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.task.core.BaseOptions
+import org.tensorflow.lite.task.vision.classifier.Classifications
+import org.tensorflow.lite.task.vision.classifier.ImageClassifier
 
 
-class ImageClassifierHelper() {
+class ImageClassifierHelper(
+    private var threshold: Float = 0.5f,
+    private var numThreads: Int = 2,
+    private var maxResults: Int = 3,
+    private val modelName: String = "cancer_classification.tflite",
+    val context: Context,
+    val listener: ClassifierListener?
+) {
+
+    private var imageClassifier: ImageClassifier? = null
+
+    init {
+        setupImageClassifier()
+    }
+
+    interface ClassifierListener {
+        fun onError(error: String)
+        fun onResults(
+            results: List<Classifications>?,
+            inferenceTime: Long
+        )
+    }
 
     private fun setupImageClassifier() {
-        // TODO: Menyiapkan Image Classifier untuk memproses gambar.
+        val optionBuilder = ImageClassifier.ImageClassifierOptions.builder()
+            .setScoreThreshold(threshold)
+            .setMaxResults(maxResults)
+
+        val baseOptionsBuilder = BaseOptions.builder().setNumThreads(numThreads)
+
+        optionBuilder.setBaseOptions(baseOptionsBuilder.build())
+
+        try {
+            imageClassifier = ImageClassifier.createFromFileAndOptions(
+                context, modelName, optionBuilder.build()
+            )
+        } catch (e: IllegalStateException) {
+            listener?.onError("Image Classifier failed to initialize. See error logs for details")
+            Log.e("Image Classifier", "TFLite failed to load model with error: " + e.message)
+        }
     }
 
     fun classifyStaticImage(imageUri: Uri) {
-        // TODO: mengklasifikasikan imageUri dari gambar statis.
+        if (imageClassifier == null) {
+            setupImageClassifier()
+        }
+
+        val imageProcessor = ImageProcessor.Builder()
+            .add(ResizeOp(224, 224, ResizeOp.ResizeMethod.NEAREST_NEIGHBOR))
+            .add(CastOp(DataType.UINT8))
+            .build()
+
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.P) {
+            val source = ImageDecoder.createSource(context.contentResolver, imageUri)
+            ImageDecoder.decodeBitmap(source)
+        } else {
+            @Suppress("DEPRECATION")
+            MediaStore.Images.Media.getBitmap(context.contentResolver, imageUri)
+        }.copy(Bitmap.Config.ARGB_8888, true)?.let { bitmap ->
+            val tensorImage = imageProcessor.process(TensorImage.fromBitmap(bitmap))
+
+            var inferenceTime = SystemClock.uptimeMillis()
+            val results = imageClassifier?.classify(tensorImage)
+
+            inferenceTime = SystemClock.uptimeMillis() - inferenceTime
+            listener?.onResults(results, inferenceTime)
+        }
     }
 
 }
--- app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
+++ app/src/main/java/com/dicoding/asclepius/view/ResultActivity.kt
@@ -2,17 +2,29 @@ package com.dicoding.asclepius.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.navArgs
 import com.dicoding.asclepius.R
 import com.dicoding.asclepius.databinding.ActivityResultBinding
 
 class ResultActivity : AppCompatActivity() {
     private lateinit var binding: ActivityResultBinding
+    private val args: ResultActivityArgs by navArgs()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_result)
 
-        // TODO: Menampilkan hasil gambar, prediksi, dan confidence score.
+        binding = ActivityResultBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.title = "Result"
+
+        args.predictionResult.apply {
+            binding.apply {
+                resultImage.setImageURI(uri)
+                resultText.text = getString(R.string.result, label, confidenceScore)
+            }
+        }
     }
 
 
